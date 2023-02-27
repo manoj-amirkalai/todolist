9dd3fde81151d75929fd08ca343963b896e66010
Manoj.A <manojamir2911@gmail.com>
2023-02-27 11:23:17 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import "./App.css";
 import InpuText from "./input";
-import {GiStairsGoal} from 'react-icons/gi';
-import {MdDeleteForever }from 'react-icons/md'
+import { GiStairsGoal } from "react-icons/gi";
+import lottie from "lottie-web";
+import { defineElement } from "lord-icon-element";
+// define "lord-icon" custom element with default properties
+defineElement(lottie.loadAnimation);
 
 function App() {
   const [items, setItems] = useState([]);
@@ -18,15 +21,27 @@ function App() {
       });
     });
   };
-  const Todo=(props)=>{
-      return(
-          <li>
-           <p><span><GiStairsGoal/></span> {props.text}</p>
-            <button className="delete" onClick={()=>deleteItems(props.id)}><MdDeleteForever/></button>
-            </li>
-        
-      )
-  }
+  const Todo = (props) => {
+    return (
+      <li>
+        <p>
+          <span>
+            <GiStairsGoal />
+          </span>{" "}
+          {props.text}
+        </p>
+        <button className="delete" onClick={() => deleteItems(props.id)}>
+          <lord-icon
+            src="https://cdn.lordicon.com/jmkrnisz.json"
+            trigger="morph"
+            colors="primary:#121331"
+            state="morph-fill"
+            style={{ width: "15px", height: "15px" }}
+          ></lord-icon>
+        </button>
+      </li>
+    );
+  };
   return (
     <div className="body">
       <InpuText additems={additems} />
